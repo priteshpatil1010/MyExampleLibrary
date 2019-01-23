d16af0928b171c123b6cbc87966058108dcb6a57
Patil Pritesh <priteshpatil1010@gmail.com>
2019-01-23 10:38:44 +0530

    sample

---
+++ patillib/src/main/java/com/herba/sdk/patillib/Patil.java
@@ -0,0 +1,29 @@
+package com.herba.sdk.patillib;
+
+public class Patil {
+
+    public int add (int a, int b)
+    {
+        return a+b;
+    }
+
+    public int subs (int a, int b)
+    {
+        return a-b;
+    }
+
+    public int multi (int a, int b)
+    {
+        return a*b;
+    }
+
+    public int div (int a, int b)
+    {
+        return a/b;
+    }
+
+    public int getStringLength(String s)
+    {
+        return s.length();
+    }
+}
